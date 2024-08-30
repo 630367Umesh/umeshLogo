22e63cf0df265f71919ce6c496defa4f0c0f84a7
630367Umesh <umeshdgumpala6303@gmail.com>
2024-08-30 15:44:26 +0530

    Umesh

---
+++ my-app/src/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <div className="diamond ">
+        <div className="text">
+          <h1>HTML & CSS</h1>
+          <p>design and build websites</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
